338c4145eb02c1f74d807925e941418426597f7c
Ivan Racetin <ivan.racetin@gmail.com>
2021-10-19 10:51:59 +0200

    commit

--- js/map.js
+++ js/map.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([34.0697,-118.4432], 17);
+var map = L.map('map').setView([16.464904,43.511316], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([34.0697,-118.4432]).addTo(map)
-		.bindPopup('The Technology Sandbox<br> Where Yoh is sitting this very moment')
+var marker = L.marker([16.464904,43.511316]).addTo(map)
+		.bindPopup('FGAG<br> Where Ivan is sitting this very moment')
 		.openPopup();	
