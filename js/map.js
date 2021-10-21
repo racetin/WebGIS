1f75d539e97feb6a14c98ac6f0b84ee816216cec
Ivan Racetin <ivan.racetin@gmail.com>
2021-10-21 14:51:31 +0200

    commit

--- js/map.js
+++ js/map.js
@@ -1,9 +1,45 @@
+//globals
+let data = [
+	{
+		'title':'Osaka',
+		'lat': 34.6937,
+		'lon': 135.5023
+	},
+	{
+		'title':'Cali',
+		'lat': 3.4516,
+		'lon': -76.5320
+	},
+	{
+		'title':'Bangkok',
+		'lat': 13.7563,
+		'lon': 100.5018
+	},
+	{
+		'title':'Tokyo',
+		'lat': 35.6762,
+		'lon': 139.6503
+	},
+	{
+		'title':'LA',
+		'lat': 34.0522,
+		'lon': -118.2437
+	}
+]
+
+
 var map = L.map('map').setView([43.511180, 16.464904], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([43.511180, 16.464904]).addTo(map)
-		.bindPopup('FGAG<br> Where Ivan is sitting this very moment')
-		.openPopup();	
+data.forEach(function(item){
+
+	var marker = L.marker([item.lat,item.lon]).addTo(map)
+	.bindPopup(item.title)
+	.openPopup();	
+
+})
+
+
